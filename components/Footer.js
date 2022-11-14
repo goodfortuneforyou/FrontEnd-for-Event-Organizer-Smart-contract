f5c9db55539eb3f5fe86620bd44631ded232b7de
DulquerSumon <m.dulquer.sumon@gmail.com>
2022-11-15 00:03:08 +0600

    footer changed

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="flex fixed bottom-0 w-full h-9 border-t-2 bg-gray-400">
       <a
-        href="https://goerli.etherscan.io/address/0xD22556110d33E7c152610D64aeC4ab9bD64BD535#code"
+        href="https://goerli.etherscan.io/address/0x11C6847e1cDCa7BC3ae4212a585b68BBC15e7c46#code"
         className="border-b-2 mt-2 border-blue-500"
       >
         Click to View on Goerli Etherscan
